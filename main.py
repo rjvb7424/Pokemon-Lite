039dc95c2d5c3b455ed86e6ff62f20c78768b347
rjvb7424 <rjvb7424@leeds.ac.uk>
2024-11-23 22:06:09 +0000

    Lack of organization lead to a reset.

--- main.py
+++
@@ -1,7 +0,0 @@
-from pokemon import pokemon
-
-pokemon1 = pokemon("Pikachu", 30, 30, [3, 1], 10)
-pokemon_opponent = pokemon("Eve", 30, 30, [2, 1], 10)
-
-print()
-pokemon1.battle(pokemon_opponent)
--- utilities.py
+++
@@ -1,7 +0,0 @@
-import time
-
-def slow_print(text: str, delay = 0.09, end='\n'):
-    for character in text:
-        print(character, end="", flush=True)
-        time.sleep(delay)
-    print(end, end='', flush=True)
