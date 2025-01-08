c83bdb771bbfd3d42fda9e2d22e9f5db7d4f4f1a
rjvb7424 <rjvb7424@leeds.ac.uk>
2025-01-08 19:21:31 +0100

    A factory method was created and contained inside a factory class. This will be the class responsible for createing pokemon objects an instatiating the pokemon class

--- pokemon_factory.py
+++ pokemon_factory.py
@@ -1,23 +1,23 @@
 from pokemon import Pokemon
 import json
 
+# Load the Pokemon data from the JSON file.
 with open("pokemon.json", "r") as file:
     pokemon_data = json.load(file)
 
-
-
+# The PokemonFactory class is responsible for creating Pokemon objects.
+# It is a Factory Method, logically this is needed as the Pokemon class is not responsible for creating Pokemon objects. 
+# The Pokemon class just provides a template for them.
 class PokemonFactory:
-    def __init__(self, id):
-        self.id = id
-
-
-    pokemon = Pokemon(
-        id=pokemon_data[id]["id"],
-        base_health_points=pokemon_data[id]["base_statistics"]["health_points"],
-        base_attack=pokemon_data[id]["base_statistics"]["attack"],
-        base_defense=pokemon_data[id]["base_statistics"]["defense"],
-        base_special_attack=pokemon_data[id]["base_statistics"]["special_attack"],
-        base_special_defense=pokemon_data[id]["base_statistics"]["special_defense"],
-        base_speed=pokemon_data[id]["base_statistics"]["speed"],
-        name=pokemon_data[id]["name"],
-    )
+    @staticmethod
+    def create_pokemon(id):
+        return Pokemon(
+            id=id,
+            base_health_points=pokemon_data[id]["base_statistics"]["health_points"],
+            base_attack=pokemon_data[id]["base_statistics"]["attack"],
+            base_defense=pokemon_data[id]["base_statistics"]["defense"],
+            base_special_attack=pokemon_data[id]["base_statistics"]["special_attack"],
+            base_special_defense=pokemon_data[id]["base_statistics"]["special_defense"],
+            base_speed=pokemon_data[id]["base_statistics"]["speed"],
+            name=pokemon_data[id]["name"],
+        )
